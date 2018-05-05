41eb61650beb475c8e4a3afad074f98c99471585
nosi <spoken0904@gmail.com>
2018-05-05 21:38:47 +0900

    DialogServiceInjection初回アップ

---
+++ WPF/Prism/DialogServiceInjection/DialogServiceInjection/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows.Input;
+
+namespace DialogServiceInjection
+{
+    /// <summary>
+    /// MainWindowViewModel
+    /// </summary>
+    public class MainWindowViewModel : BindableBase
+    {
+        IDialogService dialogService;
+        DelegateCommand showMessageCommand;
+
+        /// <summary>
+        /// メッセージを表示するコマンド
+        /// </summary>
+        public ICommand ShowMessageCommand
+        {
+            get {
+                if(showMessageCommand is null)
+                {
+                    showMessageCommand = new DelegateCommand(
+                        () =>
+                        {
+                            dialogService?.ShowMessage("Hello Prism");
+                        });
+                }
+                return showMessageCommand;
+            }
+        }
+
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindowViewModel(IDialogService dialogService)
+        {
+            this.dialogService = dialogService;
+        }
+    }
+}
---
+++ WPF/Prism/DialogServiceInjection/DialogServiceInjection/ViewModels/UserControl2ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Prism.Commands;
+using System.Windows.Input;
+
+namespace DialogServiceInjection
+{
+    class UserControl2ViewModel
+    {
+        IDialogService dialogService;
+        DelegateCommand showMessageCommand;
+
+        /// <summary>
+        /// メッセージを表示するコマンド
+        /// </summary>
+        public ICommand ShowMessageCommand
+        {
+            get
+            {
+                if (showMessageCommand is null)
+                {
+                    showMessageCommand = new DelegateCommand(
+                        () =>
+                        {
+                            dialogService?.ShowMessage("UserControl2");
+                        });
+                }
+                return showMessageCommand;
+            }
+        }
+
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public UserControl2ViewModel(IDialogService dialogService)
+        {
+            this.dialogService = dialogService;
+        }
+    }
+}
